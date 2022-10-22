733627cae2eb7f275aaac55379acfe3e5095582a
Dawid Rusnak <dawid@drcode.pl>
2022-10-22 23:47:52 +0200

    core: add event types for StreamParser

--- packages/core/src/StreamParser.ts
+++ packages/core/src/StreamParser.ts
@@ -5,6 +5,7 @@ import { BufferReader } from '@sockety/buffers';
 import { UUID } from '@sockety/uuid';
 import { FileIndexBits, FileSizeBits, PacketSizeBits, PacketTypeBits } from './constants';
 import { SocketChannel } from './SocketChannel';
+import { IncomingMessage } from './IncomingMessage';
 
 const createPacketConsumer = new BufferReader()
   .uint8('header').setInternal('header')
@@ -274,4 +275,93 @@ export class StreamParser extends Writable {
       callback(error);
     }
   }
+}
+
+export interface StreamParser {
+  addListener(event: 'message', listener: (message: IncomingMessage) => void): this;
+  on(event: 'message', listener: (message: IncomingMessage) => void): this;
+  once(event: 'message', listener: (message: IncomingMessage) => void): this;
+  prependListener(event: 'message', listener: (message: IncomingMessage) => void): this;
+  prependOnceListener(event: 'message', listener: (message: IncomingMessage) => void): this;
+  removeListener(event: 'message', listener: (message: IncomingMessage) => void): this;
+  emit(event: 'message', message: IncomingMessage): boolean;
+
+  addListener(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  on(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  once(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  prependListener(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  prependOnceListener(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  removeListener(event: 'ack' | 'revoke', listener: (id: UUID) => void): this;
+  emit(event: 'ack' | 'revoke', id: UUID): boolean;
+
+  addListener(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  on(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  once(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  prependListener(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  prependOnceListener(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  removeListener(event: 'goAway' | 'heartbeat', listener: () => void): this;
+  emit(event: 'goAway' | 'heartbeat'): boolean;
+
+  addListener(event: 'close', listener: () => void): this;
+  addListener(event: 'data', listener: (chunk: any) => void): this;
+  addListener(event: 'end', listener: () => void): this;
+  addListener(event: 'error', listener: (err: Error) => void): this;
+  addListener(event: 'pause', listener: () => void): this;
+  addListener(event: 'readable', listener: () => void): this;
+  addListener(event: 'resume', listener: () => void): this;
+  addListener(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  emit(event: 'close'): boolean;
+  emit(event: 'data', chunk: any): boolean;
+  emit(event: 'end'): boolean;
+  emit(event: 'error', err: Error): boolean;
+  emit(event: 'pause'): boolean;
+  emit(event: 'readable'): boolean;
+  emit(event: 'resume'): boolean;
+  emit(event: string | symbol, ...args: any[]): boolean;
+
+  on(event: 'close', listener: () => void): this;
+  on(event: 'data', listener: (chunk: any) => void): this;
+  on(event: 'end', listener: () => void): this;
+  on(event: 'error', listener: (err: Error) => void): this;
+  on(event: 'pause', listener: () => void): this;
+  on(event: 'readable', listener: () => void): this;
+  on(event: 'resume', listener: () => void): this;
+  on(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  once(event: 'close', listener: () => void): this;
+  once(event: 'data', listener: (chunk: any) => void): this;
+  once(event: 'end', listener: () => void): this;
+  once(event: 'error', listener: (err: Error) => void): this;
+  once(event: 'pause', listener: () => void): this;
+  once(event: 'readable', listener: () => void): this;
+  once(event: 'resume', listener: () => void): this;
+  once(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  prependListener(event: 'close', listener: () => void): this;
+  prependListener(event: 'data', listener: (chunk: any) => void): this;
+  prependListener(event: 'end', listener: () => void): this;
+  prependListener(event: 'error', listener: (err: Error) => void): this;
+  prependListener(event: 'pause', listener: () => void): this;
+  prependListener(event: 'readable', listener: () => void): this;
+  prependListener(event: 'resume', listener: () => void): this;
+  prependListener(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  prependOnceListener(event: 'close', listener: () => void): this;
+  prependOnceListener(event: 'data', listener: (chunk: any) => void): this;
+  prependOnceListener(event: 'end', listener: () => void): this;
+  prependOnceListener(event: 'error', listener: (err: Error) => void): this;
+  prependOnceListener(event: 'pause', listener: () => void): this;
+  prependOnceListener(event: 'readable', listener: () => void): this;
+  prependOnceListener(event: 'resume', listener: () => void): this;
+  prependOnceListener(event: string | symbol, listener: (...args: any[]) => void): this;
+
+  removeListener(event: 'close', listener: () => void): this;
+  removeListener(event: 'data', listener: (chunk: any) => void): this;
+  removeListener(event: 'end', listener: () => void): this;
+  removeListener(event: 'error', listener: (err: Error) => void): this;
+  removeListener(event: 'pause', listener: () => void): this;
+  removeListener(event: 'readable', listener: () => void): this;
+  removeListener(event: 'resume', listener: () => void): this;
+  removeListener(event: string | symbol, listener: (...args: any[]) => void): this;
 }
