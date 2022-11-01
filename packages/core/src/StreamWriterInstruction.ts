4d059af00a56baf39289463f4c1501ab95d09504
Dawid Rusnak <dawid@drcode.pl>
2022-11-01 12:52:12 +0100

    core: optimize StreamWriterInstruction's callbacks

--- packages/core/src/StreamWriterInstruction.ts
+++ packages/core/src/StreamWriterInstruction.ts
@@ -7,6 +7,14 @@ type SendCallback = (error: Error | null | undefined) => void;
 type WriteCallback = () => void;
 
 function createRunner(run: ExecuteWrite, sent: SendCallback, written: WriteCallback): ExecuteWrite {
+  if (sent === noop && written == noop) {
+    return run;
+  } else if (sent === noop) {
+    return (buffer) => {
+      run(buffer);
+      written();
+    };
+  }
   return (buffer) => {
     run(buffer);
     buffer.addCallback(sent);
