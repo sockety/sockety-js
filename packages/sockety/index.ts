66669ff9ab17de0764e1b37f9c9a5fbb48f07402
Dawid Rusnak <dawid@drcode.pl>
2022-11-08 16:15:58 +0100

    sockety: expose some additional types

--- packages/sockety/index.ts
+++ packages/sockety/index.ts
@@ -3,4 +3,6 @@ export * from './src/secureConnect';
 export * from './src/createServer';
 export * from './src/createSecureServer';
 export * from './src/createMessageHandler';
-export * from './src/Connection';
+export type { Connection } from './src/Connection';
+export type { Message } from './src/Message';
+export type { MessageDataStream } from '@sockety/core/src/read/MessageDataStream';
