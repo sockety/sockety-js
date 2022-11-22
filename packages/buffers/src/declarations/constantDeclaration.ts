6480e3212b454c0e2c085c9d6b9bc44511cf14c9
Dawid Rusnak <dawid@drcode.pl>
2022-11-22 17:47:45 +0100

    buffers: support Infinity value as constant

--- packages/buffers/src/declarations/constantDeclaration.ts
+++ packages/buffers/src/declarations/constantDeclaration.ts
@@ -3,10 +3,10 @@ import { createDeclaration } from '../createDeclaration';
 
 export const constantDeclaration = createDeclaration({
   read: (value: JsonValue | undefined) => (operation) => operation
-    .initialValue(value === undefined ? 'undefined' : JSON.stringify(value))
+    .initialValue(value === undefined ? 'undefined' : value === Infinity ? 'Infinity' : JSON.stringify(value))
     .resetValue(false)
     .entry(($) => `
-      ${$.set(value === undefined ? 'undefined' : JSON.stringify(value))}
+      ${$.set(value === undefined ? 'undefined' : value === Infinity ? 'Infinity' : JSON.stringify(value))}
       ${$.continue()}
     `),
 });
