7341223add02c4f362e0c016bb60a9bae1d4c9fa
Dawid Rusnak <dawid@drcode.pl>
2023-01-11 17:36:21 +0100

    core: make createNumberBytesMapper generic

--- packages/core/src/utils/createNumberBytesMapper.ts
+++ packages/core/src/utils/createNumberBytesMapper.ts
@@ -1,8 +1,13 @@
-export function createNumberBytesMapper(name: string, bytesMap: Record<number, number>): (value: number) => number {
+import { JsonValue } from 'type-fest';
+
+export function createNumberBytesMapper<T extends JsonValue>(
+  name: string,
+  bytesMap: Record<number, T>,
+): (value: number) => T {
   const available = Object.keys(bytesMap).map((bytes) => parseInt(bytes, 10));
   const code = available
-    .map((bytes) => `if(value<=${parseInt('ff'.repeat(bytes) || '0', 16)}){return ${bytesMap[bytes]}}`)
+    .map((bytes) => `if(value<=${parseInt('ff'.repeat(bytes) || '0', 16)}){return ${JSON.stringify(bytesMap[bytes])}}`)
     .join('else ');
   const raise = `else{throw new Error("The '" + ${JSON.stringify(name)} + "' value is too big");}`;
-  return new Function('value', `${code}${raise}`) as (value: number) => number;
+  return new Function('value', `${code}${raise}`) as (value: number) => T;
 }
